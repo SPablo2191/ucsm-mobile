bd437dd4dc9e5088f0f8fc23e74e9a3f5f67752c
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-01-30 18:53:16 -0300

    feat: add private, public and login modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicModule } from './modules/public/public.module';
+import { PrivateModule } from './modules/private/private.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PublicModule, PrivateModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/modules/public/public.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublicRoutingModule } from './public-routing.module';
+import { LoginModule } from './modules/login/login.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PublicRoutingModule, LoginModule],
+})
+export class PublicModule {}
