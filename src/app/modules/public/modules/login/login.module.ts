fe87e698539fc51074f54ec226aea9fa6be09c80
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-01-30 20:07:49 -0300

    feat: add background image in login

--- src/app/modules/public/modules/login/login.module.ts
+++ src/app/modules/public/modules/login/login.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { LoginComponent } from './login.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, LoginRoutingModule],
+  declarations: [LoginComponent],
+  imports: [CommonModule, LoginRoutingModule, FormsModule, IonicModule, ReactiveFormsModule],
 })
 export class LoginModule {}
