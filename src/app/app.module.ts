6cfd8809427b3dea46c57d0c36def6ec94422648
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-03 17:38:44 -0300

    feature: add animation to card container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicModule } from './modules/public/public.module';
 import { PrivateModule } from './modules/private/private.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PublicModule, PrivateModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    PublicModule,
+    PrivateModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/shared/card-container/card-container.component.ts
+++ src/app/shared/card-container/card-container.component.ts
@@ -1,13 +1,15 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AnimationController, IonCard, IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'card-container',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
   template: `
-    <ion-card class="card min-h-screen m-0">
+    <ion-card class="card min-h-screen m-0" #card>
       <ion-card-content>
         <ng-content></ng-content>
       </ion-card-content>
@@ -15,13 +17,31 @@ import { IonicModule } from '@ionic/angular';
   `,
   styles: `
   .card{
-	  background: white;
-	   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20);
-	    border-top-left-radius: 25px;
-		 border-top-right-radius: 25px;
-		  border-bottom-right-radius: 5px;
-		   border-bottom-left-radius: 5px;
+	background: white;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20);
+	border-top-left-radius: 25px;
+	border-top-right-radius: 25px;
+	border-bottom-right-radius: 5px;
+	border-bottom-left-radius: 5px;
   }
   `,
 })
-export class CardContainerComponent {}
+export class CardContainerComponent implements AfterViewInit {
+  @ViewChild(IonCard, { read: ElementRef }) card: ElementRef<HTMLIonCardElement> | undefined;
+  constructor(private animationCtrl: AnimationController) {}
+  ngAfterViewInit(): void {
+    this.toggleVisibility();
+  }
+
+  toggleVisibility() {
+    if (this.card != undefined) {
+      let animation = this.animationCtrl
+        .create()
+        .addElement(this.card.nativeElement)
+        .duration(1000) // Ajusta la duración según tus preferencias
+        .fromTo('transform', 'translateY(100%)', 'translateY(0)') // Mueve desde abajo hacia arriba
+        .fromTo('opacity', '0', '1');
+      animation.play();
+    }
+  }
+}
