f7f1f62c3f37258ac6a229275cfb18e28d6bbf01
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-08 23:46:46 -0300

    feat: add background color in career profile

--- src/app/shared/ui/ui-tabs/ui-tabs.component.ts
+++ src/app/shared/ui/ui-tabs/ui-tabs.component.ts
@@ -17,7 +17,7 @@ import { IonicModule } from '@ionic/angular';
           <ion-icon name="cash"></ion-icon>
           Deuda
         </ion-tab-button>
-        <ion-tab-button (click)="goToHome()" class="bg-white">
+        <ion-tab-button (click)="goToHome()" class="home-button">
           <ion-icon name="home"></ion-icon>
           Inicio
         </ion-tab-button>
@@ -32,7 +32,13 @@ import { IonicModule } from '@ionic/angular';
       </ion-tab-bar>
     </ion-tabs>
   `,
-  styles: ``,
+  styles: `
+  .home-button{
+	background: linear-gradient(180deg, #05BE6A 0%, #099957 100%);
+	color : white;
+	width: 60px; height: 50px;
+	border-radius: 9999px;
+  }`,
 })
 export class UiTabsComponent {
   constructor(private router: Router) {}
