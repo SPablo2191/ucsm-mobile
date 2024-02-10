5124a7fccd911aa815de9c1d8eec199c06556602
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-10 13:31:33 -0300

    feat: add header in grade and schedule

--- src/app/modules/private/modules/grade/grade.module.ts
+++ src/app/modules/private/modules/grade/grade.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { GradeRoutingModule } from './grade-routing.module';
 import { GradeComponent } from './grade.component';
+import { HeaderPageComponent } from 'src/app/shared/header-page/header-page.component';
 
 @NgModule({
   declarations: [GradeComponent],
-  imports: [CommonModule, GradeRoutingModule],
+  imports: [CommonModule, GradeRoutingModule, HeaderPageComponent],
 })
 export class GradeModule {}
--- src/app/modules/private/modules/schedule/schedule.module.ts
+++ src/app/modules/private/modules/schedule/schedule.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
+import { HeaderPageComponent } from 'src/app/shared/header-page/header-page.component';
 
 @NgModule({
   declarations: [ScheduleComponent],
-  imports: [CommonModule, ScheduleRoutingModule],
+  imports: [CommonModule, ScheduleRoutingModule, HeaderPageComponent],
 })
 export class ScheduleModule {}
