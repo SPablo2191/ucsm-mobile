c24effae8d85026fc00bb6c9ce2946ab9b81d1ca
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-09 21:25:43 -0300

    feat: subject card with ion

--- src/app/shared/cards/subject-card/subject-card.component.ts
+++ src/app/shared/cards/subject-card/subject-card.component.ts
@@ -2,22 +2,32 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
-  selector: 'app-subject-card',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'subject-card',
   standalone: true,
   imports: [IonicModule],
   template: `
-    <ion-card>
+    <ion-card class="card ">
       <ion-card-header>
-        <ion-card-title>Titulo</ion-card-title>
-        <ion-card-subtitle>subtitulo</ion-card-subtitle>
+        <ion-card-title>
+          <div class="flex justify-content-between align-items-center mx-2">
+            <p class="font-bold text-sm">En curso</p>
+            <p class="font-italic text-xs">Ver Materia ></p>
+          </div>
+        </ion-card-title>
+        <ion-card-subtitle>
+          <p class="font-bold text-xl text-left">Introducción a la Programación</p>
+        </ion-card-subtitle>
       </ion-card-header>
 
       <ion-card-content>
-        <p>body</p>
-        <ng-content></ng-content>
+        <p></p>
       </ion-card-content>
     </ion-card>
   `,
-  styles: ``,
+  styles: `
+  .card{
+	  background: linear-gradient(180deg, #05BE6A 0%, #099957 100%); box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px;
+  }`,
 })
 export class SubjectCardComponent {}
