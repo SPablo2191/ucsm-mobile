463c1dd3b0c2b80beaf5851bf2e04bbbb9f4f4ff
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-11 13:15:07 -0300

    fix: add currency translation private module

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import LocaleEsPe from '@angular/common/locales/es-PE';
+import { registerLocaleData } from '@angular/common';
 import { PrivateRoutingModule } from './private-routing.module';
 import { UiTabsComponent } from 'src/app/shared/ui/ui-tabs/ui-tabs.component';
+registerLocaleData(LocaleEsPe);
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, PrivateRoutingModule, UiTabsComponent],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-PE' }],
 })
 export class PrivateModule {}
