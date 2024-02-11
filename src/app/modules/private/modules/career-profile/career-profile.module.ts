dcc557a18824f153ed2ed76eea9ff1548bf07f49
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-11 11:35:32 -0300

    fix: show currency in spanish format

--- src/app/modules/private/modules/career-profile/career-profile.module.ts
+++ src/app/modules/private/modules/career-profile/career-profile.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CareerProfileRoutingModule } from './career-profile-routing.module';
@@ -8,6 +8,9 @@ import { TabCardComponent } from 'src/app/shared/cards/tab-card/tab-card.compone
 import { SubjectCardComponent } from 'src/app/shared/cards/subject-card/subject-card.component';
 import { UiCardComponent } from 'src/app/shared/ui/ui-card/ui-card.component';
 import { LinkButtonComponent } from 'src/app/shared/link-button/link-button.component';
+import LocaleEsPe from '@angular/common/locales/es-PE';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(LocaleEsPe);
 
 @NgModule({
   declarations: [CareerProfileComponent],
@@ -20,5 +23,6 @@ import { LinkButtonComponent } from 'src/app/shared/link-button/link-button.comp
     UiCardComponent,
     LinkButtonComponent,
   ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-PE' }],
 })
 export class CareerProfileModule {}
