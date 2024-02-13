0f17c508a25acb73403f6a1adaafc602c4a81d73
SPablo2191 <pablosandoval2191@gmail.com>
2024-02-13 16:48:55 -0300

    feat: add dropdown

--- src/app/modules/private/modules/grade/modules/subject/subject.module.ts
+++ src/app/modules/private/modules/grade/modules/subject/subject.module.ts
@@ -5,9 +5,10 @@ import { SubjectRoutingModule } from './subject-routing.module';
 import { SubjectComponent } from './subject.component';
 import { IonicModule } from '@ionic/angular';
 import { CardContainerComponent } from 'src/app/shared/card-container/card-container.component';
+import { UiDropdownComponent } from 'src/app/shared/ui/ui-dropdown/ui-dropdown.component';
 
 @NgModule({
   declarations: [SubjectComponent],
-  imports: [CommonModule, SubjectRoutingModule, IonicModule, CardContainerComponent],
+  imports: [CommonModule, SubjectRoutingModule, IonicModule, CardContainerComponent, UiDropdownComponent],
 })
 export class SubjectModule {}
---
+++ src/app/shared/ui/ui-dropdown/ui-dropdown.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'ui-dropdown',
+  standalone: true,
+  imports: [IonicModule],
+  template: `
+    <ion-select label="SEMESTRE-PAR-2023">
+      <ion-select-option value="apple">Apple</ion-select-option>
+      <ion-select-option value="banana">Banana</ion-select-option>
+      <ion-select-option value="orange">Orange</ion-select-option>
+    </ion-select>
+  `,
+  styles: ``,
+})
+export class UiDropdownComponent {}
