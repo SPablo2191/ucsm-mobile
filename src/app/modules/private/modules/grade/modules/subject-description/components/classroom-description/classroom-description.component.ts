f02c29700409dad4c5ae4274f27ff8aa5de59b64
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-15 14:49:20 -0300

    feat: add times in subject description

--- src/app/modules/private/modules/grade/modules/subject-description/components/classroom-description/classroom-description.component.ts
+++ src/app/modules/private/modules/grade/modules/subject-description/components/classroom-description/classroom-description.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'classroom-description',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, IonicModule],
   template: `
     <div class=" text-xl text-center main-title-section font-bold">{{ main_title }}</div>
     <div class="flex justify-content-center">
@@ -12,13 +14,34 @@ import { Component, Input } from '@angular/core';
         <div class="col">
           <div class="text-xl font-bold text-center">{{ title_left }}</div>
           <div class="text-lg font-bold text-center">{{ description_left }}</div>
-          <div class="col">
-            <div></div>
+          <div class="col" *ngIf="start_hour">
+            <div class="flex justify-content-center align-items-center">
+              <ion-icon name="time-outline" style="font-size: 20px; margin-right: 4px;"></ion-icon>
+              Inicio: {{ start_hour }}
+            </div>
+          </div>
+          <div class="col" *ngIf="end_hour">
+            <div class="flex justify-content-center align-items-center">
+              <ion-icon name="time-outline" style="font-size: 20px; margin-right: 4px;"></ion-icon>
+              Fin: {{ end_hour }}
+            </div>
           </div>
         </div>
         <div class="col">
           <div class="text-xl font-bold text-center">{{ title_right }}</div>
           <div class="text-lg font-bold text-center">{{ description_right }}</div>
+          <div class="col" *ngIf="start_hour">
+            <div class="flex justify-content-center align-items-center">
+              <ion-icon name="time-outline" style="font-size: 20px; margin-right: 4px;"></ion-icon>
+              Inicio: {{ start_hour }}
+            </div>
+          </div>
+          <div class="col" *ngIf="end_hour">
+            <div class="flex justify-content-center align-items-center">
+              <ion-icon name="time-outline" style="font-size: 20px; margin-right: 4px;"></ion-icon>
+              Fin: {{ end_hour }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -35,4 +58,6 @@ export class ClassroomDescriptionComponent {
   @Input() title_right = '';
   @Input() description_left = '';
   @Input() description_right = '';
+  @Input() start_hour!: string;
+  @Input() end_hour!: string;
 }
