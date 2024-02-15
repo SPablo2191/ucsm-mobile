c0f7139d41041a1b52dc7397723fa1ac62802020
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-15 12:07:59 -0300

    feat: add subject description style

--- src/app/modules/private/modules/grade/modules/subject-description/subject-description.component.ts
+++ src/app/modules/private/modules/grade/modules/subject-description/subject-description.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-subject-description',
   templateUrl: './subject-description.component.html',
-  styleUrl: './subject-description.component.css',
+  styleUrl: './subject-description.component.scss',
 })
 export class SubjectDescriptionComponent {}
--- src/app/modules/private/modules/grade/modules/subject-description/subject-description.module.ts
+++ src/app/modules/private/modules/grade/modules/subject-description/subject-description.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SubjectDescriptionRoutingModule } from './subject-description-routing.module';
 import { SubjectDescriptionComponent } from './subject-description.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [SubjectDescriptionComponent],
-  imports: [CommonModule, SubjectDescriptionRoutingModule],
+  imports: [CommonModule, SubjectDescriptionRoutingModule, IonicModule],
 })
 export class SubjectDescriptionModule {}
