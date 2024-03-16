c494543fc4bb8e2dbf6fc8f2e0df471c070e7d86
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-16 00:34:11 -0300

    feat: add ion module

--- src/app/modules/private/modules/schedule/schedule.module.ts
+++ src/app/modules/private/modules/schedule/schedule.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
 import { HeaderPageComponent } from 'src/app/shared/header-page/header-page.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [ScheduleComponent],
-  imports: [CommonModule, ScheduleRoutingModule, HeaderPageComponent],
+  imports: [CommonModule, ScheduleRoutingModule, IonicModule, HeaderPageComponent],
 })
 export class ScheduleModule {}
