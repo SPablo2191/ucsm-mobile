c4c29cafe94646e37dd9c9ba55aad69e587df556
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-16 00:27:27 -0300

    feat: add event page

--- src/app/modules/private/modules/event/event.component.ts
+++ src/app/modules/private/modules/event/event.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrl: './event.component.css',
+  styleUrl: './event.component.scss',
 })
-export class EventComponent {}
+export class EventComponent {
+  imagePath = '/assets/images/casa-marianista.jpg';
+}
--- src/app/modules/private/modules/event/event.module.ts
+++ src/app/modules/private/modules/event/event.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EventRoutingModule } from './event-routing.module';
 import { EventComponent } from './event.component';
+import { IonicModule } from '@ionic/angular';
+import { CardContainerComponent } from 'src/app/shared/card-container/card-container.component';
 
 @NgModule({
   declarations: [EventComponent],
-  imports: [CommonModule, EventRoutingModule],
+  imports: [CommonModule, EventRoutingModule, IonicModule, CardContainerComponent],
 })
 export class EventModule {}
