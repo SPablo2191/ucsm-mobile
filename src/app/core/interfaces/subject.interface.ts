fe822fe8a0dd2e1870d94b714e633b0f240fa30c
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-26 11:26:30 -0300

    feat: add get semester and grades

--- src/app/core/interfaces/subject.interface.ts
+++ src/app/core/interfaces/subject.interface.ts
@@ -2,4 +2,6 @@ import { Observable } from 'rxjs';
 
 export interface ISubjectService<T> {
   getSubjects(code: string): Observable<T>;
+  getSemesters(code: string): Observable<any>;
+  getGrades(code: string): Observable<T>;
 }
