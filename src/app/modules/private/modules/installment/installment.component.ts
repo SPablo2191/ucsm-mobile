dc014f845b2d854759984cca923d4ac07ebd10aa
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-06-09 17:40:51 -0300

    feat: add installments

--- src/app/modules/private/modules/installment/installment.component.ts
+++ src/app/modules/private/modules/installment/installment.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, map } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { PartialDebt } from 'src/app/project/interfaces/debt.interface';
-import { PartialStudent } from 'src/app/project/interfaces/student.interface';
-import { DebtService } from 'src/app/project/services/php/debt.service';
+import { DebtService } from 'src/app/project/services/python/debt.service';
 
 @Component({
   selector: 'app-installment',
@@ -10,30 +9,17 @@ import { DebtService } from 'src/app/project/services/php/debt.service';
   styleUrl: './installment.component.css',
 })
 export class InstallmentComponent implements OnInit {
-  debt: PartialDebt = {};
-  protected student!: PartialStudent;
+  debt$!: Observable<PartialDebt>;
   protected subscriptions$: Subscription = new Subscription();
   constructor(private debtService: DebtService) {}
   ionViewDidLeave(): void {
     this.subscriptions$.unsubscribe();
   }
   ngOnInit(): void {
-    let studentStoraged = localStorage.getItem('student');
-    if (studentStoraged) {
-      this.student = JSON.parse(studentStoraged);
-    }
     this.getInstallments();
   }
   getInstallments() {
-    this.subscriptions$.add(
-      this.debtService
-        .getDebt(this.student.identification_document || '')
-        .pipe(
-          map((debt) => {
-            this.debt = debt;
-          }),
-        )
-        .subscribe(),
-    );
+    let enrollmentId = localStorage.getItem('enrollment_id');
+    if (enrollmentId) this.debt$ = this.debtService.getDebt(enrollmentId);
   }
 }
