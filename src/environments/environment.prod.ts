25add72862ccce064fe8eed5726df40bba4ae933
SPablo2191 <pablosandoval2191@gmail.com>
2024-06-29 12:33:43 -0300

    fix: environment vars for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ibas.ucsm.edu.pe/WS_PWA/webService.php/',
+  //   apiUrl: 'https://ibas.ucsm.edu.pe/WS_PWA/webService.php/',
+  apiUrl: 'https://ucsm-mobile-api.onrender.com/api',
 };
